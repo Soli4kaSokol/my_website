0510c96dbf32a33ba465080cd7274a151cb8fa73
Solomiya <solomija2001@gmail.com>
2019-05-14 22:11:47 +0000

    fix

--- js/index.js
+++ js/index.js
@@ -8,17 +8,5 @@ function submitForm(e) {
   })
   .then(response => response.text())
   .then(html => document.querySelector('.server-response')
-   .innerHTML = html);
-}
-document.querySelector('.feedback textarea[type=submit]').addEventListener('click', submitForm);
-
-function submitForm(e) {
- e.preventDefault();
- fetch('form.php', {
-   method: 'POST',
-   body: new FormData(document.querySelector('.forms'))
-  })
-  .then(response => response.text())
-  .then(html => document.querySelector('.server-response')
   .innerHTML = html);
 }
